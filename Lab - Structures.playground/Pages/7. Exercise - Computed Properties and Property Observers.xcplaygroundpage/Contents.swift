e7f51e73acf5e12c4d1d6f7c3450a8dde61f2075
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-10-03 14:05:37 -0400

    SHE DOES NOT WANT TO GO OVER THERE (Context: you asked marlin is she wanted to come over there)

--- Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
@@ -23,23 +23,41 @@ rec.Area()
  Create an instance of `Height` and then change one of its properties. Print out the other property to ensure that it was adjusted accordingly.
  */
 struct Height {
-    var heightInInches: Double
+    var heightInInches: Double {
+        didSet {
+//            let cm = heightInInches * 2.54
+//            if heightInCentimeters != cm{
+//                heightInCentimeters = cm
+//            }
+            if heightInInches != oldValue{
+                heightInCentimeters = heightInInches/2.54
+            }
+        }
+    }
     
-    var heightInCentimeters: Double
+    var heightInCentimeters: Double {
+        didSet {
+            if heightInCentimeters != oldValue{
+                heightInInches = heightInCentimeters * 2.54
+            }
+            
+        }
+    }
     
     init(heightInInches: Double) {
         self.heightInInches = heightInInches
         self.heightInCentimeters = heightInInches*2.54
-    }
-    didSet {
-        if heightInInches != oldValue
-    }
+        
+        }
+    
     
     init(heightInCentimeters: Double) {
         self.heightInCentimeters = heightInCentimeters
         self.heightInInches = heightInCentimeters/2.54
     }
+
 }
+
 
 
 
--- Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -8,8 +8,22 @@ struct User {
     var userName: String
     var email: String
     var age: Int
+    static var currentUser = User(userName: "Jose", email: "Jose2006@gmail.com", age: 10)
+    
+    
+    
+    static func logIn(p1:User)
+    {
+        
+        currentUser = p1
+        print("\(p1) has logged in!")
+    }
 }
 
+print(User.currentUser.userName)
+var newUser = User(userName: "ANTIGUA", email: "Catlhater@gmail.com", age: 20)
+User.logIn(p1: newUser )
+print(User.currentUser.userName)
 
 /*:
  There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
